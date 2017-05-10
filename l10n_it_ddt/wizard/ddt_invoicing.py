d7a71f7407de6a28c95f356a9a1610e2338a860b
Alex Comba <alex.comba@agilebg.com>
2017-05-10 09:38:04 +0200

    [FIX] l10n_it_ddt: fix test_flake8

--- l10n_it_ddt/wizard/ddt_invoicing.py
+++ l10n_it_ddt/wizard/ddt_invoicing.py
@@ -1,12 +1,6 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-#    Author: Alessandro Camilli <alessandrocamilli@openforce.it>
-#    Copyright (C) 2017 Openforce di Camilli Alessandro (http://www.openforce.it)
-#
-#    License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
-#
-##############################################################################
+# Copyright 2017 Alessandro Camilli - Openforce
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 
 from odoo import fields, models, api, _
